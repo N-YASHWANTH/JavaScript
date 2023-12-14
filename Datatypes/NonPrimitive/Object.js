fbbdeac793a691d9fa66030cde4cde095d86e1df
N Yashwanth <yashsai8629@gmail.com>
2023-12-14 10:19:21 +0530

    [N Yashwanth]Added Datatypes

---
+++ Datatypes/NonPrimitive/Object.js
@@ -0,0 +1,5 @@
+emp1={firstname:"N", lastname:"Yashwanth", email:"yash98798@gmail.com", phno:5487963214}
+
+console.log(typeof emp1);
+console.log(typeof emp1.phno);
+console.log(emp1.email);
---
+++ Datatypes/Primitive/Undefined.js
@@ -0,0 +1,8 @@
+let a
+
+console.log(a);
+console.log(typeof a);
+
+b=undefined
+console.log(b);
+console.log(typeof b);
