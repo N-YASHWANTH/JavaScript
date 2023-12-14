9564c2b67b5acd7f6f3d48c071510f2d8faa5fd0
N Yashwanth <yashsai8629@gmail.com>
2023-12-14 14:30:11 +0530

    [N Yashwanth]Added push() method

---
+++ ArrayMethods/push.js
@@ -0,0 +1,5 @@
+let arr=[2,4,6,8]
+console.log(arr.push(10));//5
+// console.log(arr.toString());
+
+//push() method actually adds the element in the last position but it returns the length of the array after adding the new element.
