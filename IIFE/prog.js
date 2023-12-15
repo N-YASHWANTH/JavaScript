c63dd5ef6e9033a352f962018acf93773864030e
N Yashwanth <yashsai8629@gmail.com>
2023-12-15 12:57:08 +0530

    [N Yashwanth]Added IIFE program

--- IIFE/prog.js
+++ IIFE/prog.js
@@ -1,5 +1,11 @@
 //IIFE -> Immediate Invoke Function Expression
 
+// (function(x,y,z){
+//     return x+y+z;
+// })
+// sum=(10,20,30)
+// console.log(sum);
+
 (function(x,y,z){
     console.log(x+y+z);
 })
