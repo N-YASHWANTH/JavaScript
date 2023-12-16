773e58db19e9c5338229e232beb7e3d99dd33194
N Yashwanth <yashsai8629@gmail.com>
2023-12-16 10:32:09 +0530

    [N Yashwanth]Modified reduce program

--- Reduce/prog.js
+++ Reduce/prog.js
@@ -10,4 +10,4 @@ let arr=[1,2,3,4,5]
 let sum=arr.reduce((total,num)=>total+num)
 
 console.log("Original Array-------------",arr);
-console.log("Sum of array elements------------",sum);
+console.log("Reduced Value------------",sum);
