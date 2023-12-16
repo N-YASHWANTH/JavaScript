898fb3ad8af34cb9bb25b6cba423260e89ed85be
N Yashwanth <yashsai8629@gmail.com>
2023-12-16 10:30:31 +0530

    [N Yashwanth]Modified filter program

--- Filter/prog.js
+++ Filter/prog.js
@@ -1,6 +1,6 @@
 let ages = [32, 33, 16, 40];
 result = ages.filter((age)=>age>=18);
 
-console.log("Modified array---------",result)
+console.log("Filtered array---------",result)
 console.log("Original array----------",ages)
 
