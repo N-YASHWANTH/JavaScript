dbf79b3f130dc7686f825f445f0f4872a5cfdf75
N Yashwanth <yashsai8629@gmail.com>
2023-12-16 09:52:04 +0530

    [N Yashwanth]Modified mapmethod program

--- mapmethod/prog.js
+++ mapmethod/prog.js
@@ -1,9 +1,11 @@
 let arr=[1,2,3,4]
 newarr=arr.map((num)=>num*num)
 
-console.log(newarr)
+console.log("Original Array-------------",arr);
+console.log("Modified Array-------------",newarr)
 
 let numbers=[1,2,3,4,5,6]
 newnumbers=numbers.map((num)=>num+10)
 
-console.log(newnumbers);
+console.log("Original Array-------------",numbers);
+console.log("Modified Array-------------",newnumbers);
