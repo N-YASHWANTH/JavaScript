bb4a2efe0b7086e47ad81112467a47c9cc146e37
N Yashwanth <yashsai8629@gmail.com>
2023-12-16 10:26:01 +0530

    [N Yashwanth]Added reduce

---
+++ Reduce/prog.js
@@ -0,0 +1,13 @@
+const numbers = [175, 50, 25];
+
+let diff=numbers.reduce((total,num)=>total-num)
+
+console.log("Original Array--------------",numbers);
+console.log("Difference of array elements------------",diff)
+
+let arr=[1,2,3,4,5]
+
+let sum=arr.reduce((total,num)=>total+num)
+
+console.log("Original Array-------------",arr);
+console.log("Sum of array elements------------",sum);
